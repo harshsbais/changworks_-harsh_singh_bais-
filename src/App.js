4a0021e5414f3393a0a4b3af44f9ebecbd0cf819
harshsbais <harshsbais5@gmail.com>
2022-01-11 20:05:41 +0530

    feat: add basic function of hotkey and confetti

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { HotKeys } from "react-hotkeys";
+import confetti from 'canvas-confetti';
 function App() {
+  const check = (e) => {
+    confetti();
+  }
+  const keyMap = {
+    changworks: ["c", "h", "a", "n", "g", "w", "o", "r", "k", "s"],
+  };
+  const handlers = {
+    changworks: check
+  };
   return (
-    <div>
-      Hello world
-    </div>
+    <HotKeys keyMap={keyMap} handlers={handlers} style={{
+      outline: "none"
+    }}>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh'
+      }}>
+        Type "changworks"
+      </div>
+    </HotKeys>
   );
 }
 
